f7bac0030c788ff8b7b653621510ad1f473dc999
Pablo Vergara <vergarapablo2001@gmail.com>
2024-02-22 14:53:36 -0600

    start kiosko screen

--- src/app/kiosko/page.tsx
+++ src/app/kiosko/page.tsx
@@ -1,3 +1,36 @@
+// "use client";
+
+import { useEffect, useState } from "react";
+
+import ProductKiosko from "@/components/ProductKiosko";
+
 export default function Kiosko() {
-  return <h1>From Kiosko /kiosko</h1>;
+  return (
+    <div className="">
+      <div className="grid grid-cols-3 gap-12 max-w-4xl mx-auto my-12">
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+        <ProductKiosko />
+      </div>
+    </div>
+  );
 }
